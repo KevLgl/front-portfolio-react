924707302d4cbff980d7026b6ff43d71ee47de1c
laguel <kevin.laguel@digiwin.fr>
2021-07-10 17:35:40 +0200

    verification des données du form

--- src/components/Pdfform.js
+++ src/components/Pdfform.js
@@ -1,8 +1,49 @@
 
 import React from 'react';
-import { useFormik, Field } from 'formik';
+import { useFormik, Formik } from 'formik';
+import * as Yup from 'yup';
 
 const Pdfform = () => {
+        const validationSchema = Yup.object().shape({
+                firstName: Yup.string()
+                        .min(2, 'Trop court!')
+                        .required('Le prenom est obligatoire'),
+                lastName: Yup.string()
+                        .min(2, 'Trop court!')
+                        .required('Le nom est obligatoire'),
+                birthdate: Yup.string()
+                        .required('La date de naissance est obligatoire'),
+                place_of_birth: Yup.string()
+                        .required('Votre lieu de naissance est obligatoire'),
+                sexe: Yup.string()
+                        .required('Votre sexe est obligatoire'),
+                height: Yup.number()
+                        .integer('Votre taille doit être renseignée en CM')
+                        .required('Votre taille est obligatoire'),
+                lateralite: Yup.string()
+                        .required('Votre latéralité est obligatoire'),
+                adress: Yup.string()
+                        .required('Votre adresse est obligatoire'),
+                cp: Yup.string()
+                        .required('Votre code postal est obligatoire'),
+                country: Yup.string()
+                        .required('Votre ville est obligatoire'),
+                email: Yup.string()
+                        .email('Adresse e-mail invalide')
+                        .required('l\'adresse e-mail est obligatoire'),
+                phone_number1: Yup.number()
+                        .integer('Mauvais format de numéro')
+                        .required('Votre numéro est obligatoire'),
+                phone_number2: Yup.number()
+                        .integer('Mauvais format de numéro'),
+                phone_number3: Yup.number()
+                        .integer('Mauvais format de numéro'),
+                reglement: Yup.boolean()
+                        .oneOf([true], "Vous devez accepter le règlement intérieur "),
+                chartre: Yup.boolean()
+                        .oneOf([true], "Vous devez accepter la chartre du club"),
+              });
+
         const formik = useFormik({
                 initialValues: {
                   firstName: '',
@@ -26,7 +67,9 @@ const Pdfform = () => {
                 onSubmit: values => {
                   alert(JSON.stringify(values, null, 2));
                 },
+                validationSchema
               });
+
         return (
                 <form onSubmit={formik.handleSubmit}>
                         <label htmlFor="firstName">Prenom:</label>
@@ -37,6 +80,9 @@ const Pdfform = () => {
                         onChange={formik.handleChange}
                         value={formik.values.firstName}
                         />
+                        {
+                                formik.errors.firstName && <span className="text-xs text-red-500 -bottom-6">{formik.errors.firstName}</span>
+                        }
 
                         <br />
 
@@ -48,6 +94,9 @@ const Pdfform = () => {
                         onChange={formik.handleChange}
                         value={formik.values.lastName}
                         />
+                         {
+                                formik.errors.lastName && <span className="text-xs text-red-500 -bottom-6">{formik.errors.lastName}</span>
+                        }
 
                         <br />
 
@@ -60,6 +109,10 @@ const Pdfform = () => {
                         value={formik.values.birthdate}
                         />
 
+                        {
+                                formik.errors.birthdate && <span className="text-xs text-red-500 -bottom-6">{formik.errors.birthdate}</span>
+                        }
+
                         <br />
 
                         <label htmlFor="place_of_birth">Lieu de naissance:</label>
@@ -70,7 +123,9 @@ const Pdfform = () => {
                         onChange={formik.handleChange}
                         value={formik.values.place_of_birth}
                         />
-
+                        {
+                                formik.errors.place_of_birth && <span className="text-xs text-red-500 -bottom-6">{formik.errors.place_of_birth}</span>
+                        }
                         <br />
 
                         <label htmlFor="sexe">Sexe:</label>
@@ -90,7 +145,9 @@ const Pdfform = () => {
                         onChange={formik.handleChange}
                         />
                         <span> Homme </span>
-
+                        {
+                                formik.errors.sexe && <span className="text-xs text-red-500 -bottom-6">{formik.errors.sexe}</span>
+                        }
                         <br />
 
                         <label htmlFor="height">Taille:</label>
@@ -101,6 +158,10 @@ const Pdfform = () => {
                         onChange={formik.handleChange}
                         value={formik.values.height}
                         />
+
+                        {
+                                formik.errors.height && <span className="text-xs text-red-500 -bottom-6">{formik.errors.height}</span>
+                        }
 
                         <br />
 
@@ -122,6 +183,10 @@ const Pdfform = () => {
                         />
                         <span> Droitier </span>
 
+                        {
+                                formik.errors.lateralite && <span className="text-xs text-red-500 -bottom-6">{formik.errors.lateralite}</span>
+                        }
+
                         <br />
 
                         <label htmlFor="adress">Adresse:</label>
@@ -132,6 +197,10 @@ const Pdfform = () => {
                         onChange={formik.handleChange}
                         value={formik.values.adress}
                         />
+
+                        {
+                                formik.errors.adress && <span className="text-xs text-red-500 -bottom-6">{formik.errors.adress}</span>
+                        }
 
                         <br />
 
@@ -144,6 +213,10 @@ const Pdfform = () => {
                         value={formik.values.cp}
                         />
 
+                        {
+                                formik.errors.cp && <span className="text-xs text-red-500 -bottom-6">{formik.errors.cp}</span>
+                        }       
+
                         <br />
 
                         <label htmlFor="country">Ville:</label>
@@ -154,6 +227,10 @@ const Pdfform = () => {
                         onChange={formik.handleChange}
                         value={formik.values.country}
                         />
+
+                        {
+                                formik.errors.country && <span className="text-xs text-red-500 -bottom-6">{formik.errors.country}</span>
+                        }
 
                         <br />
 
@@ -166,6 +243,10 @@ const Pdfform = () => {
                         value={formik.values.phone_number1}
                         />
 
+                        {
+                                formik.errors.phone_number1 && <span className="text-xs text-red-500 -bottom-6">{formik.errors.phone_number1}</span>
+                        }
+
                         <br />
 
                         <label htmlFor="phone_number2">Numero telephone domicile:</label>
@@ -176,6 +257,10 @@ const Pdfform = () => {
                         onChange={formik.handleChange}
                         value={formik.values.phone_number2}
                         />
+                        
+                        {
+                                formik.errors.phone_number2 && <span className="text-xs text-red-500 -bottom-6">{formik.errors.phone_number2}</span>
+                        }
 
                         <br />
 
@@ -188,6 +273,10 @@ const Pdfform = () => {
                         value={formik.values.phone_number3}
                         />
 
+                        {
+                                formik.errors.phone_number3 && <span className="text-xs text-red-500 -bottom-6">{formik.errors.phone_number3}</span>
+                        }
+
                         <br />
 
                         <label htmlFor="email">Adresse e-mail:</label>
@@ -198,7 +287,9 @@ const Pdfform = () => {
                         onChange={formik.handleChange}
                         value={formik.values.email}
                         />
-
+                        {
+                                formik.errors.email && <span className="text-xs text-red-500 -bottom-6">{formik.errors.email}</span>
+                        }
                         <br />
 
                         <label htmlFor="reglement">Je déclare avoir pris connaissance du règlement intérieur du club et en accepter les conditions</label>
